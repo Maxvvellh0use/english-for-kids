e487c0ace70735231c8b5d0ef908bd7ee823ecd6
Maksim Pavlov <pavlovmark100@gmail.com>
2020-04-14 01:27:10 +0400

    feat: realize rotate cards without translation

--- src/js/components/Card/Categories.js
+++ src/js/components/Card/Categories.js
@@ -5,9 +5,10 @@ import {
     LINKS_CATEGORIES,
     MAIN_PAGE_CONTAINER,
     CATEGORY_PAGE_CONTAINER,
-    TITLE_CATEGORY
+    TITLE_CATEGORY,
+    CARD_BUTTON,
+    CARD_BODY
 } from '../../constants/constants';
-let clickLink = 0;
 
 export default class Categories {
   constructor() {
@@ -25,14 +26,14 @@ export default class Categories {
   appendContentsToCategory(indexCard) {
       cards[indexCard].map((elem, index) => {
           CARD_CATEGORIES[index].innerHTML = '';
-          CARD_CATEGORIES[index].insertAdjacentHTML('afterbegin', `<div class="card-body category_card_body"><p class="card-text">${elem.word}</p>`);
-          CARD_CATEGORIES[index].insertAdjacentHTML('afterbegin', `<img src="${elem.image}" class="categories__cards_img">`)
-
+          CARD_CATEGORIES[index].insertAdjacentHTML('afterbegin', `<button class="card-body__button"><img class="svg_button" src="src/img/rotate.svg"></button>`);
+          CARD_CATEGORIES[index].insertAdjacentHTML('afterbegin', `<div class="card-body category_card_body category_text"><p class="card-text">${elem.word}</p>`);
+          CARD_CATEGORIES[index].insertAdjacentHTML('afterbegin', `<img src="${elem.image}" class="categories__cards_img">`);
+          // CARD_CATEGORIES[index].insertAdjacentHTML('afterbegin', `<div class="card-body category_card_body card-translate" style="display: none"><p>${elem.translation}</p>`);
       });
   }
 
   openCategoryPage(link) {
-      console.log(link.textContent);
           switch (link.id || link.textContent ) {
               case 'Action (set A)':
                   this.appendContentsToCategory(0);
@@ -79,17 +80,48 @@ export default class Categories {
               this.openCategoryPage(link);
           });
       });
-      this.playingWords()
+      this.playingWords();
   }
 
   playingWords() {
-      CARD_CATEGORIES.forEach(card => {
+      const cardText = document.querySelectorAll('.card-text');
+      CARD_CATEGORIES.forEach((card, index) => {
           card.addEventListener('click', () => {
               let audio = new Audio(`src/audio/${card.textContent}.mp3`);
               audio.play().then(() => audio.preload = 'auto');
-              console.log(card.textContent);
+              this.rotateListeners(card);
+          })
+      });
+
+  }
+
+  rotateListeners(card) {
+      const cardButtons = document.querySelectorAll('.card-body__button');
+      const cardText = document.querySelectorAll('.card-text');
+      const cardTranslation = document.querySelectorAll('.card-translate');
+      cardButtons.forEach((button, index) => {
+          button.addEventListener('click', (event) => {
+              event.preventDefault();
+              return this.rotateToTranslate(button, cardText, index);
+          });
+          button.parentElement.addEventListener('mouseleave', (event) => {
+              event.preventDefault();
+              return this.backRotate(button);
           })
       })
+  }
+
+  rotateToTranslate(button, cardText, index) {
+      console.log(index);
+      // cardText[index].style.display = 'none';
+      button.style.opacity = '0';
+      button.parentElement.style.transform = 'rotateY(180deg)';
+  }
+
+
+  backRotate(button) {
+      button.style.opacity = '1';
+      button.parentElement.style.transform = 'rotateY(0deg)';
   }
 
 }
--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -13,7 +13,9 @@ export const HAMBURGER_LINKS = document.querySelectorAll('.hamburger_menu__list_
 // categoriesPage:
 export const CATEGORY_PAGE_CONTAINER = document.getElementById('category_page');
 export const CARD_CATEGORIES = document.querySelectorAll('.card_category');
-// export const CARD_CATEGORIES_TEXT = document.querySelectorAll('.category-card-body p');
+export const CARD_BODY = document.querySelectorAll('.category_card_body');
 export const TITLE_CATEGORY = document.getElementById('header_title');
+
+
 
 
